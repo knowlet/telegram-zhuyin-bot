939805638868ca773d3926ad59cf87d99c206d29
knowlet <knowlet3389@gmail.com>
2016-07-12 21:05:17 +0800

    Add some check

--- index.js
+++ index.js
@@ -6,19 +6,31 @@ const bot = new Bot(token, {polling: true});
 
 bot.on('message', function (msg) {
     const fromId = msg.chat.id;
-    console.log(`From: ${fromId} Message: ${msg.text}`);
-    // https://inputtools.google.com/request?text=%7C%E4%BD%A0%E5%A5%BD%2C%3B%3D&itc=zh-hant-t-i0-und&num=13&cp=0&cs=1&ie=utf-8&oe=utf-8&app=demopage
-    r.get('https://inputtools.google.com/request')
-    .query({
-        text: msg.text.replace(/ /g, '='),
-        itc: 'zh-hant-t-i0-und'
-    })
-    .on('error', err => console.log(`ERROR! ${err}`))
-    .end((err, res) => {
-        if (res.ok) {
-            let zhuyin = JSON.parse(res.text)[1][0][1][0];
-            console.log(zhuyin);
-            bot.sendMessage(fromId, zhuyin);
-        }
-    });
+    if (msg.text) {
+        console.log(`From: ${fromId} Message: ${msg.text}`);
+        // https://inputtools.google.com/request?text=%7C%E4%BD%A0%E5%A5%BD%2C%3B%3D&itc=zh-hant-t-i0-und&num=13&cp=0&cs=1&ie=utf-8&oe=utf-8&app=demopage
+        r.get('https://inputtools.google.com/request')
+        .query({
+            text: msg.text.replace(/ /g, '='),
+            itc: 'zh-hant-t-i0-und',
+            num: 13,
+            cp: 0,
+            cs: 1,
+            ie: 'utf-8',
+            oe: 'utf-8'
+        })
+        .on('error', err => console.log(`ERROR! ${err}`))
+        .end((err, res) => {
+            if (res.ok) {
+                let zhuyin = JSON.parse(res.text)[1][0][1][0];
+                if (zhuyin === undefined)
+                    zhuyin = msg.text;
+                console.log(zhuyin);
+                bot.sendMessage(fromId, zhuyin);
+            }
+        });
+    }
+    else {
+        bot.sendMessage(fromId, 'Text Message Only!');
+    }
 });
