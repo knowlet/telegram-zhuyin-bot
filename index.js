a21e15fa0818955953837deba0969aed549f212c
knowlet <knowlet3389@gmail.com>
2018-05-31 14:58:05 +0800

    Fix kanji multi output choosen

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Bot = require('node-telegram-bot-api');
 const r = require('superagent');
-const bopomo = require("tobopomo.js");
-const config = require('./config.js');
-const token = config.token;
+const bopomo = require('tobopomo.js');
+const {token} = require('./config.js');
 const bot = new Bot(token, {polling: true});
 
 const gInputPrefix = message => (message.replace(/ /g, '=') + '=').replace(/,/g, encodeURIComponent(','));
@@ -26,21 +25,30 @@ const toBopomo = (fromId, message) => {
         .end((err, res) => {
             if (res.ok) {
                 let zhuyin = JSON.parse(res.text)[1][0][1][0];
-                if (zhuyin === undefined) {
-                    try {
-                        zhuyin = tobopomo(message).tokanji().join('');
-                    } catch (e) {
-                        zhuyin = message;
-                    }
+                let kanji = '';
+                try {
+                    kanji = tobopomo(message).tokanji().map(b => b[0]).join``;
+                } catch (e) {
+                    console.error(e)
+                    kanji = '';
                 }
+                console.log('kanji:')
+                console.log(kanji);
+                console.log('zhuyin:')
                 console.log(zhuyin);
+                if (zhuyin === undefined) {
+                    zhuyin = kanji;
+                }
+                else {
+                    zhuyin = zhuyin.length >= kanji.length ? zhuyin : kanji
+                }
                 bot.sendMessage(fromId, zhuyin);
             }
         });
     } catch (e) {
         console.log(e);
     }
-}
+};
 
 bot.on('message', msg => toBopomo(msg.chat.id, msg.text));
 bot.on('inline_query', msg => {
